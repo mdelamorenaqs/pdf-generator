76777e4ab827aa052c2c9b178eed6f9a03fdfdef
mdelamorenaqs <mdelamorena@qseguridad.es>
2025-07-22 17:25:25 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: './', // ESTA LINEA ES CLAVE PARA EVITAR 404 EN VERCEL
-})
