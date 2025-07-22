13d302113fb3bd8f6fc355566c106e46265077c1
mdelamorenaqs <mdelamorena@qseguridad.es>
2025-07-22 18:53:21 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      external: ['pdfmake/build/pdfmake']
+    }
+  }
 });
